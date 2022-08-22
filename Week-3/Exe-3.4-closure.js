3e89558a6b579a90923a53a96a01d61f8d5af91b
nishant <nishantsinha.er@gmail.com>
2022-08-22 16:36:11 +0530

    updates

--- Week-3/Exe-3.4-closure.js
+++ Week-3/Exe-3.4-closure.js
@@ -14,10 +14,9 @@ function createStack()
 //const st= createStack();
 
 const stack=createStack();
-const st=stack;
 stack.push(10);
 stack.push(5);
 stack.pop();// => 5
 stack.items;// => [10]
-console.log(st.items);
-console.log(stack.items); // [10,100,1000];// Encapsulationbroken!functioncreateStack() {// Write your code here...}conststack=createStack();stack.push(10);stack.push(5);stack.pop();// => 5stack.items;// => undefined
+console.log(stack.items); 
+console.log(items); //undefined
